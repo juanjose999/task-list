338e1af12c2f7f51ff58732e3b70b64d873957cc
juanjose999 <juanjose000012@gmail.com>
2025-02-11 16:31:27 -0500

    initial commit

---
+++ src/test/java/com/task_list/TaskListAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskListAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
